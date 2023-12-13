fe0920470c9357f46a5888fcbaa01bd7b891077f
peanut-lover <jihoson94@gmail.com>
2023-12-13 23:36:06 +0900

    D-Day processing

--- api.js
+++ api.js
@@ -26,3 +26,25 @@ export const getPRList = async () => {
     state: "open",
   });
 };
+
+export const removeLabel = async (number, name) => {
+  const { data: labels } = await global.octokit.rest.issues.removeLabel({
+    owner: global.owner,
+    repo: global.repo,
+    issue_number: number,
+    name,
+  });
+
+  return labels;
+};
+
+export const addLabels = async (number, names) => {
+  const { data: labels } = await global.octokit.rest.issues.addLabels({
+    owner: global.owner,
+    repo: global.repo,
+    issue_number: number,
+    labels: names,
+  });
+
+  return labels;
+};
